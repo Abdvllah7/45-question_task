be36e34dd02addd28f5ffa81e2435c0ac95d4737
Abdvllah7 <czczcsxvdg@gmail.com>
2024-05-06 05:29:51 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+let personName = "\t  Muhammad Abdullah \n";
+console.log("Name with whitespace:", personName);
+let strippedName = personName.trim();
+console.log("stripped name:", strippedName);
---
+++ index.ts
@@ -0,0 +1,4 @@
+let personName: string = "\t  Muhammad Abdullah \n";
+console.log("Name with whitespace:", personName)
+let strippedName: string = personName.trim()
+console.log("stripped name:", strippedName)
